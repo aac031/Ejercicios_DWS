71ed655150297693147c0cca728170f604d8fb4e
aac031 <113922069+aac031@users.noreply.github.com>
2022-11-24 21:08:19 +0100

    v2

--- v1/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['logueado']) || !$_SESSION['logueado']) {
-    header("Location: login.php");
-} else {
-    unset($_SESSION['logueado']);
-    header("Location: login.php");
-}
