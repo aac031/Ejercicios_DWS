1f0629aeb63f54442b79bd817f4041009e913b08
aac031 <113922069+aac031@users.noreply.github.com>
2022-11-24 21:09:50 +0100

    v3

---
+++ v2/modificar_persona.php
@@ -0,0 +1,56 @@
+<?php
+if (!isset($_SESSION['logueado']) || !$_SESSION['logueado']) {
+    header("Location: login.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Modificar contacto</title>
+</head>
+
+<body>
+    <?php
+    include "conexion.php";
+    $sql = "SELECT * FROM contacto_persona";
+    $resultado = mysqli_query($conexion, $sql);
+    ?>
+    <h2>Lista de Contactos de persona:</h2>
+    <table class="minimalistBlack">
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Apellidos</th>
+                <th>Direccion</th>
+                <th>Telefono</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            while ($filas = mysqli_fetch_assoc($resultado)) {
+            ?>
+                <tr>
+                    <td><?php echo $filas['nombre'] ?></td>
+                    <td><?php echo $filas['apellidos'] ?></td>
+                    <td><?php echo $filas['direccion'] ?></td>
+                    <td><?php echo $filas['telefono'] ?></td>
+                    <td>
+                        <?php echo "<a href='modificar_persona_2.php?id=" . $filas['id'] . "'>Modificar contacto</a>"; ?>
+                    </td>
+                </tr>
+            <?php
+            }
+            ?>
+    </table>
+    <?php
+    mysqli_close($conexion);
+    ?>
+</body>
+
+</html>
